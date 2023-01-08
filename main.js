5c4053101a1fb3e347aa9c00280450f04e86b957
Gasstin <agustinruiz0202@gmail.com>
2023-01-07 22:49:03 -0300

    Proyecto publicado

---
+++ main.js
@@ -0,0 +1,102 @@
+// Variables
+let tarjetasdestapadas = 0;
+let tarjeta1 = null;
+let tarjeta2 = null;
+let primerResultado = null;
+let segundoResultado = null;
+let movimientos = 0;
+let aciertos = 0;
+let temporizador = false;
+let timer = 30;
+let timerInicio = 30;
+let tiempoRegrasivoId = null;
+
+//Ids html
+let mostrarMovs = document.getElementById("movimientos");
+let mostrarAciertos = document.getElementById("aciertos");
+let mostrarTiempo = document.getElementById("tiempor");
+
+// Numeros aleatorios
+let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+numeros = numeros.sort(()=>{return Math.random()-0.5})
+//console.log(numeros);
+
+//Funciones
+function contarTiempo(){
+    tiempoRegrasivoId = setInterval(()=>{
+        timer--;
+        mostrarTiempo.innerHTML = `Tiempo: ${timer} segundos`;
+        if(timer==0){
+            clearInterval(tiempoRegrasivoId);
+            bloquearTarjetas();
+            mostrarTiempo.innerHTML = `Tiempo finalizado 💀`
+        }
+    },1000)
+}
+
+function bloquearTarjetas(){
+    for (let i = 0; i <= 15; i++){
+        let tarjetaBloqueada = document.getElementById(i);
+        tarjetaBloqueada.innerHTML = `<img src="images/${numeros[i]}.png" alt="">`
+        tarjetaBloqueada.disabled = true;
+
+    }
+}
+
+
+
+// Funcion principal
+function destapar(id){
+
+    if(temporizador == false){
+        contarTiempo();
+        temporizador = true;
+    }
+
+    tarjetasdestapadas++;
+    //console.log(tarjetasdestapadas);
+
+    if(tarjetasdestapadas == 1){
+        //mostrar primer numero
+        tarjeta1 = document.getElementById(id);
+        primerResultado = numeros[id];
+        tarjeta1.innerHTML = `<img src="images/${primerResultado}.png" alt="">`;
+        //Deshabilitar primer boton
+        tarjeta1.disabled = true;
+    }else if(tarjetasdestapadas == 2){
+        // Mostrar segundo numero
+        tarjeta2 = document.getElementById(id);
+        segundoResultado = numeros[id];
+        tarjeta2.innerHTML = `<img src="images/${segundoResultado}.png" alt="">`;
+        tarjeta2.disabled = true;
+
+        //Incrementar movimientos
+        movimientos++;
+        mostrarMovs.innerHTML = `Movimientos: ${movimientos}`;
+
+        if(primerResultado == segundoResultado){
+            tarjetasdestapadas = 0;
+
+            //Aumentar aciertos
+            aciertos++;
+            mostrarAciertos.innerHTML = `Aciertos: ${aciertos}`
+
+            if(aciertos == 8){
+                clearInterval(tiempoRegrasivoId);
+                mostrarAciertos.innerHTML = `Aciertos: ${aciertos}✨`
+                mostrarTiempo.innerHTML = `Felicitaciones! Tardaste ${timerInicio - timer} segundos`
+                mostrarMovs.innerHTML = `Movimientos: ${movimientos}😎`
+            }
+
+        }else{
+            //Mostrar resultados y volver a tapar
+            setTimeout(()=>{
+                tarjeta1.innerHTML = " ";
+                tarjeta2.innerHTML = " ";
+                tarjeta1.disabled = false
+                tarjeta2.disabled = false
+                tarjetasdestapadas = 0;
+            },800);
+        }
+    }
+}
